3b32a5e00937e703676d3719ce0c51e284029bd6
Ujjwal Goyal <hasher@LAP-MACA-810.local>
2019-07-31 16:36:07 +0530

    Updates

---
+++ precache-manifest.9981557cd50d10be92bc47be3bf426cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ccf9a17c6ee48b30f2c542a77928ff",
+    "url": "/artworks/index.html"
+  },
+  {
+    "revision": "4601863bd5a0963b201b",
+    "url": "/artworks/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/artworks/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "4601863bd5a0963b201b",
+    "url": "/artworks/static/js/main.e5ec2dcd.chunk.js"
+  },
+  {
+    "revision": "d6bbdcba801a3a2c8bae",
+    "url": "/artworks/static/js/runtime~main.a67415b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/artworks/static/media/logo.5d5d9eef.svg"
+  }
+]);
